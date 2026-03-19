cd0fdd84b0083eec7bcf4742331e96e2d0849efc
GrazielleMoraes19 <graziellemoraes09@gmail.com>
2026-03-19 16:32:29 -0300

    sesisp

---
+++ src/main/java/com/escola/sesi/AlunoRepository.java
@@ -0,0 +1,7 @@
+package com.escola.sesi;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AlunoRepository extends JpaRepository<Aluno, Long> {
+
+}
---
+++ src/main/java/com/escola/sesi/AlunoService.java
@@ -0,0 +1,34 @@
+package com.escola.sesi;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class AlunoService {
+ 
+
+     private final AlunoRepository repository;
+
+    public AlunoService(AlunoRepository repository) {
+        this.repository = repository;
+    }
+
+    public List<Aluno> listarTodos() {
+        return repository.findAll();
+    }
+
+    public Aluno salvar(Aluno aluno) {
+        return repository.save(aluno);
+    }
+
+    public void deletar(Long id) {
+        repository.deleteById(id);
+    }
+
+    public Aluno buscarPorId(Long id) {
+        return repository.findById(id).orElse(null);
+    }
+
+
+}
